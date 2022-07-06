4845927b9c34c04cb9c6b30b1cc41da36622c2ac
Sihle Calana <CLNSIH001@myuct.ac.za>
2022-07-06 20:54:56 +0200

    included a variation method to mutate genes

--- NSmEDEA.py
+++ NSmEDEA.py
@@ -6,7 +6,6 @@ class MedeaController(Controller):
 
     def __init__(self, wm):
         super().__init__(wm)
-        #self.weights = None
         self.gList = dict()
         self.rob = Pyroborobo.get()
         self.next_gen_every_it = 400
@@ -20,35 +19,25 @@ class MedeaController(Controller):
             if initCol == 0: self.genome = [255,0,0]
             elif initCol == 1: self.genome = [0,255,0]
             elif initCol == 2: self.genome = [0,0,255]
+            print("[",self.genome[0],",",self.genome[1],",",self.genome[2],"]")
             self.set_color(*self.genome)
 
     def step(self):
         self.next_gen_in_it -= 1
-        if self.next_gen_in_it < 0 or self.deactivated:
+        if self.next_gen_in_it <= 0 or self.deactivated:
             self.new_generation()
             self.next_gen_in_it = self.next_gen_every_it
 
         if self.deactivated:
             self.set_color(0, 0, 0)
-            self.genome = []
-            self.gList = dict()
             self.set_translation(0)
             self.set_rotation(0)
         else:
-            #self.set_color(0, 0, 255)
             # Movement
-            #inputs = self.get_inputs()
-            #trans_speed, rot_speed = inputs @ self.weights
             self.set_translation(1)
             self.set_rotation(np.random.choice([0, -0.5, 0.5]))
             # Share weights
             self.broadcast()
-
-    def nb_inputs(self):
-        return (1  # bias
-                + self.nb_sensors * 3  # cam inputs
-                + 2  # landmark inputs
-                )
 
     def broadcast(self):
         for robot_controller in self.get_all_robot_controllers():
@@ -56,52 +45,43 @@ class MedeaController(Controller):
                 robot_controller.exchange(self.id, self.genome)
 
     def exchange(self,robID, genes):
-        self.gList[robID] = genes
-
-    def get_inputs(self):
-        dists = self.get_all_distances()
-        is_robots = self.get_all_robot_ids() != -1
-        is_walls = self.get_all_walls()
-        is_objects = self.get_all_objects() != -1
-
-        robots_dist = np.where(is_robots, dists, 1)
-        walls_dist = np.where(is_walls, dists, 1)
-        objects_dist = np.where(is_objects, dists, 1)
-
-        landmark_dist = self.get_closest_landmark_dist()
-        landmark_orient = self.get_closest_landmark_orientation()
-
-        inputs = np.concatenate([[1], robots_dist, walls_dist, objects_dist, [landmark_dist, landmark_orient]])
-        #assert(len(inputs) == self.nb_inputs())
-        return inputs
+        self.gList[robID] = genes.copy()
 
     def new_generation(self):
         if self.gList:
             randomRob = np.random.choice(list(self.gList.keys()))
             selection  = self.gList[randomRob]
-            #new_weights = self.received_weights[new_weights_key]
-            # mutation 1
-            #selection[randomRob%3] += 32;
-            #variation = [x%256 for x in selection]
-            # mutation 2
-            variation = np.random.normal(selection, 0.5)
-            variation = [abs(int(x)) for x in variation]
-            #print("NEW COLOUR IS...", "[",variation[0],",",variation[1],",",variation[2],"]")
-            self.genome = variation
+            print("MY Parents Are", self.id, "and", randomRob)
+            print("Dad:", "[",self.genome[0],",",self.genome[1],",",self.genome[2],"]")
+            print("Mom:", "[",selection[0],",",selection[1],",",selection[2],"]")
+            # mutation 3
+            #variation = np.random.normal(selection, 0.5)
+            #variation = [abs(int(x)) for x in variation]
+            mutation = self.variation(self.genome, selection.copy())
+            print("MY PHENOTYPE IS...", "[",mutation[0],",",mutation[1],",",mutation[2],"]\n")
+            self.genome = mutation
             self.gList.clear()
             self.deactivated = False
             self.set_color(*self.genome)
         else:
             self.deactivated = True
 
+    def variation(self, parent1, parent2):
+        for i in range(3): parent2[i] += parent1[i]
+        v1 = [x%256 for x in parent2]
+        v2 = [x//2 for x in parent2]
+        mutate = [None]*3
+        for i in range(3):
+            mutate[i] = (v1[i] + v2[i])//2
+        return mutate
+    
     def inspect(self, prefix=""):
-        output = "inputs: \n" + str(self.get_inputs()) + "\n\n"
-        output += "received weights from: \n"
+        output = "received weights from: \n"
         output += str(list(self.gList.keys()))
         return output
 
 def main():
-    rob = Pyroborobo.create("config/talking_robots.properties",
+    rob = Pyroborobo.create("config/NSmEDEA.properties",
                             controller_class=MedeaController)
     rob.start()
     rob.update(10000)
